0b6442dcec141d89d7424e26bd37e7f7ffac9d01
Munnarhh <okenyimax10@gmail.com>
2024-09-02 06:24:10 +0100

    Update

--- src/main/java/com/example/demo/walletapp/controller/WelcomeController.java
+++ src/main/java/com/example/demo/walletapp/controller/WelcomeController.java
@@ -1,10 +1,12 @@
 package com.example.demo.walletapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class WelcomeController {
     @GetMapping("/")
     public Map<String, String> welcome() {
